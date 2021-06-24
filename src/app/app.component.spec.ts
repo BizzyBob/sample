f98e60d401e9a7f3e598d781b036439fc4bbf82a
Bob Loper <bob@bobloper.com>
2021-06-24 11:27:55 -0400

    chore: remove unused properties and tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('[AppComponent]', () => {
+  let componentUnderTest: AppComponent;
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+    TestBed.configureTestingModule({
+      providers: [ AppComponent ]
+    });
+
+    componentUnderTest = TestBed.inject(AppComponent);
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'sample-with-students'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('sample-with-students');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sample-with-students app is running!');
-  });
+  describe('INIT', () => {
+    it('INIT: should create the app', () => {
+      expect(componentUnderTest).toBeTruthy();
+    });
+  })
 });
