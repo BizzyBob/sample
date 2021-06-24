74b9de1523c42f704c7a358ca704d9f4928050a7
Bob Loper <bob@bobloper.com>
2021-06-24 11:25:16 -0400

    chore: configure karma reporting

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,8 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
+      require('karma-mocha-reporter'),
+      require('karma-jasmine-diff-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -21,9 +22,6 @@ module.exports = function (config) {
       },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/sample-with-students'),
       subdir: '.',
@@ -32,7 +30,18 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['jasmine-diff', 'mocha'],
+    jasmineDiffReporter: {
+      color: {
+        expectedBg: 'bgMagenta',
+        expectedWhitespaceBg: 'bgMagenta',
+        actualBg: 'bgBlue',
+        actualWhitespaceBg: 'bgBlue'
+      }
+    },
+    mochaReporter: {
+      output: 'minimal'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
