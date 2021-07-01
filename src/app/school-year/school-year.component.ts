5df33450524dd79882fd85904f9e65fa68d0d501
Bob Loper <bob@bobloper.com>
2021-07-01 17:31:24 -0400

    refactor: move transformation code to own function

--- src/app/school-year/school-year.component.ts
+++ src/app/school-year/school-year.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
-import { combineLatest, Observable, Subject } from 'rxjs';
+import { combineLatest, Subject } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { Student } from '../models';
 import { StudentService } from '../services/student.service';
@@ -11,29 +11,12 @@ import { StudentService } from '../services/student.service';
   styleUrls: ['./school-year.component.sass']
 })
 export class SchoolYearComponent {
-
-  private year$ = new Subject<number>();
-  
   @ViewChild(MatDrawer) drawer!: MatDrawer;
-
+  private year$ = new Subject<number>();
   public columns = ['year', 'attendence', 'gpa'];
 
-  public statsByYear$: Observable<SchoolYearSummary[]> = this.studentService.getStudents().pipe(
-    map(allStudents => {
-      const allYears = allStudents.reduce<number[]>((all, s) => all.concat(s.yearsAttended), []);
-      const uniqueYears = [... new Set(allYears)].sort();
-
-      return uniqueYears.map(year => {
-        const students = allStudents.filter(s => s.yearsAttended.some(y => y === year));
-        const totalGpa = students.reduce((sum, s) => sum + s.gpaByYear.get(year)!, 0);
-
-        return {
-          year,
-          students,
-          averageGpa: totalGpa / students.length,
-        };
-      });
-    })
+  public statsByYear$ = this.studentService.getStudents().pipe(
+    map(studentsToSummary)
   );
 
   public details$ = combineLatest([this.year$, this.statsByYear$]).pipe(
@@ -46,7 +29,6 @@ export class SchoolYearComponent {
   }
 
   constructor(private studentService: StudentService) { }
-
 }
 
 interface SchoolYearSummary {
@@ -54,3 +36,19 @@ interface SchoolYearSummary {
   students   : Student[];
   averageGpa : number;
 }
+
+function studentsToSummary(allStudents: Student[]): SchoolYearSummary[] {
+  const allYears = allStudents.reduce<number[]>((all, s) => all.concat(s.yearsAttended), []);
+  const uniqueYears = [...new Set(allYears)].sort();
+
+  return uniqueYears.map(year => {
+    const students = allStudents.filter(s => s.yearsAttended.some(y => y === year));
+    const totalGpa = students.reduce((sum, s) => sum + s.gpaByYear.get(year)!, 0);
+
+    return {
+      year,
+      students,
+      averageGpa: totalGpa / students.length
+    };
+  });
+};
